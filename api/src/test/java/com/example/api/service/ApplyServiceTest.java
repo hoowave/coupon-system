e4aa683d3bffb9dc8e528d9dec22d3b23bc96e10
hoowave <ma5ter@naver.com>
2023-11-20 16:47:31 +0900

    레이스 컨디션 발생

---
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -0,0 +1,57 @@
+package com.example.api.service;
+
+import com.example.api.repository.CouponRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class ApplyServiceTest {
+    @Autowired
+    private ApplyService applyService;
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Test
+    public void 한번만응모() {
+        applyService.apply(1L);
+
+        long count = couponRepository.count();
+
+        assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    public void 여러개응모() throws InterruptedException {
+        int threadCount = 1000;
+        // 동시에 접근하기 위해
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+        // 2개 이상의 Thread가 공유 자원에 엑세스를 하고 작업을 하려고 할 때 발생되는
+        // 문제점을 레이스 컨디션이라고 함
+        long count = couponRepository.count();
+        assertThat(count).isEqualTo(100);
+
+    }
+
+}
